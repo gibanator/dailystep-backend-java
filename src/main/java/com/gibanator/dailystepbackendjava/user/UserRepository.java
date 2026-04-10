cff1c437986e58b68dcd0b27bcd61d39af36d0cf
gibanator <georgy.gibizov@gmail.com>
2026-04-10 16:18:26 +0300

    Users and Categories For now, without auth

--- src/main/java/com/gibanator/dailystepbackendjava/user/UserRepository.java
+++ src/main/java/com/gibanator/dailystepbackendjava/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.gibanator.dailystepbackendjava.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
--- src/main/java/com/gibanator/dailystepbackendjava/user/dto/CreateUserResponse.java
+++ src/main/java/com/gibanator/dailystepbackendjava/user/dto/CreateUserResponse.java
@@ -1,7 +1,5 @@
 package com.gibanator.dailystepbackendjava.user.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
