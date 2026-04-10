a01365be4ee111ca1b94965de271118835943d33
gibanator <georgy.gibizov@gmail.com>
2026-04-10 17:52:02 +0300

    Users and Categories For now, without auth

--- src/main/java/com/gibanator/dailystepbackendjava/category/dto/CreateCategoryRequest.java
+++ src/main/java/com/gibanator/dailystepbackendjava/category/dto/CreateCategoryRequest.java
@@ -1,12 +1,12 @@
 package com.gibanator.dailystepbackendjava.category.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateCategoryRequest {
-    @NotNull
+    @NotBlank(message = "Category name is required.")
     private String name;
 }
