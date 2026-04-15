cb82888e6140a2df1698fa86a728dd370877a5f2
giba <georgy.gibizov@gmail.com>
2026-04-15 19:55:42 +0300

    added contextLoads again

--- src/test/java/com/gibanator/dailystepbackendjava/DailystepBackendJavaApplicationTests.java
+++ src/test/java/com/gibanator/dailystepbackendjava/DailystepBackendJavaApplicationTests.java
@@ -2,9 +2,14 @@ package com.gibanator.dailystepbackendjava;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DailystepBackendJavaApplicationTests {
 
+    @Test
+    void contextLoads() {
+    }
 
 }
